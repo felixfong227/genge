0c00f7455416a04098f1e6d0ffda103cd8b81d41
Felix Fong <moongod101@hotmail.com>
2017-04-03 20:11:35 +0800

    Remove the testing code

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,6 @@ const cwd = process.cwd();
 const cliColor = require('cli-color');
 let runApp = false;
 const logs = require('./coreModule/logs');
-logs('Howdy', 'error')
 const color = {
     error: cliColor.red
     ,warning: cliColor.yellow
